0a5a097ce33b0ea46be7de97d620abca6301e058
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-18 16:52:42 -0300

    project created

---
+++ src/test/java/com/projects/businesstock/BusinesstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.businesstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinesstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
