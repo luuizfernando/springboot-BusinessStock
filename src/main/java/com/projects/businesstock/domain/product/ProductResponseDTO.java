7f32d84046c04dfcdc42437eeaabd4a140996db4
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-20 14:06:44 -0300

    feat/ Postgre connection

--- src/main/java/com/projects/businesstock/domain/product/ProductResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.projects.businesstock.domain.product;
-
-public record ProductResponseDTO(String id, String name, Double price, ProductCategory category) {
-    public ProductResponseDTO(Product product) {
-        this(product.getId(), product.getName(), product.getPrice(), product.getCategory());
-    }
-}
