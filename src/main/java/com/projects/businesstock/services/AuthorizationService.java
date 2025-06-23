40b5883f29e1d4d98184a5d5f5a819274a2ea4c6
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-22 22:00:15 -0300

    Update AuthorizationService.java

--- src/main/java/com/projects/businesstock/services/AuthorizationService.java
+++ src/main/java/com/projects/businesstock/services/AuthorizationService.java
@@ -17,6 +17,6 @@ public class AuthorizationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByUsername(username);
-    }   
+    }
 
 }
